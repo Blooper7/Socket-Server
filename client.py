ff4b60aa1600b8af8dea42b63cf25cea3fc8561b
Sam <Samjpoyner@icloud.com>
2024-09-19 13:45:08 +0000

    Allowed for client port choice in command line via args
    ex: python client.py 40001

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
+import sys
 
 def run_client(server_ip, server_port, client_details=("127.0.0.1",40001)):
+    
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #client = socket.socket(("127.0.0.1",40001), socket.SOCK_STREAM)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -24,7 +26,10 @@ def run_client(server_ip, server_port, client_details=("127.0.0.1",40001)):
         
     client.close()
     print("Connection closed")
-    
-run_client("127.0.0.1", 8000)
-#run_client("127.0.0.1", 8000, ("127.0.0.1", 41110))
+
+port=40001
+if sys.argv[1] != None:
+	port=int(sys.argv[1])
+#run_client("127.0.0.1", 8000)
+run_client("127.0.0.1", 8000, ("127.0.0.1", port))
 #                              ^If not specified, defaulted to ("127.0.0.1", 40001)
